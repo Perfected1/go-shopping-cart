6aa0b8406f89f7f0bfb4acabeea09b56e309be7d
Perfected1 <chikennamadim65@gmail.com>
2026-05-31 06:51:41 -0700

    basic cart and item - test run

--- main.go
+++ main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Simple Shopping Cart Started")
+
+	cart := Cart{}
+
+	item := Item{
+		Name:  "Book",
+		Price: 10.5,
+		Qty:   2,
+	}
+
+	cart.Items = append(cart.Items, item)
+
+	fmt.Println("Cart Items:", cart.Items)
 }
